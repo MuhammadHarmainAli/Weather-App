d64a462ac1fe021a229a7395da63725ffb462f7b
muhammadharmainali <codebyharmain@gmail.com>
2026-09-06 04:08:01 +0500

    script update

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-const cityInput = document.querySelector('#cityInput');
-const searchBtn = document.querySelector('#searchBtn');
+const cityInput = document.querySelector("#cityInput");
+const searchBtn = document.querySelector("#searchBtn");
+
+const weatherData = [];
 
 const getInputValue = () => {
-  return cityInput.value.trim()
-}
+  return cityInput.value.trim();
+};
 
-searchBtn.addEventListener('click', () => {
+const validateCity = (searchCity) => {
+  if (searchCity === "") {
+    return "City is required";
+  }
+};
+
+const fetchWeather = (city) => {
+  const YOUR_API_KEY = "42fa97ab1ae20f4a773c0dc725689221";
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${YOUR_API_KEY}&units=metric`;
+
+  fetch(apiUrl);
+};
+
+searchBtn.addEventListener("click", () => {
   const searchCity = getInputValue();
-})
+
+  alert(validateCity(searchCity));
+
+  fetchWeather(searchCity);
+});
